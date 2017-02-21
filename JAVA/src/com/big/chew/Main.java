b58799a52f68dad6132774debe9f155e33946d2c
John Garza <johnjoegarza@hotmail.com>
2017-02-21 15:42:53 -0800

    Updated Code

--- JAVA/src/com/big/chew/Main.java
+++ JAVA/src/com/big/chew/Main.java
@@ -1,21 +1,32 @@
 package com.big.chew;
 
+import javax.swing.UIManager;
+
 /**
  * <h1>Chewy Recipe Application
  * <h1>Recipe application that provides a UI for viewing stored recipes.
  * <p>
  * <b>Note:</b> Current conception will use embedded Data Base
  * 
- * @author John J. Garza
+ * @author Johh J. Garza
  * @version 0.2
  * @since 2016-6-2017
  */
 public class Main {
 	public static void main(String[] args) {
+		try {
+
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 
 				OpeningFrame op = new OpeningFrame();
+				op.getUI(true);
 			}
 		});
 	}
